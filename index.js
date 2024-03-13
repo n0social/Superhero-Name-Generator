65c5269016da77d6edbc10eb6b1de713a07fcab1
n0social <38169198+n0social@users.noreply.github.com>
2024-03-12 19:12:11 -0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,15 @@
+//var generateName = require("sillyname");
+//var sillyName = generateName();
+
+//type module was just added to the package.json
+//file so now all I have to do is import
+
+import superheroes from "superheroes"
+const name = superheroes.random();
+
+console.log(`I am ${name}!`);
+
+//console.log(sillyName);
+//console.log(heroName);
+
+//console.log(`Hello, my name is ${sillyName}.`);
